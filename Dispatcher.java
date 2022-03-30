2c940f64b5978c828840c4d4b1f473c91346eb4c
Eamon Niknafs <en@bu.edu>
2022-03-29 20:21:09 -0400

    fixed default timeout and formatting

--- Dispatcher.java
+++ Dispatcher.java
@@ -12,31 +12,34 @@ public class Dispatcher {
     Queue<String> WorkQueue = new LinkedList<String>();
     BlockingQueue<Runnable> WorkerQueue = new ArrayBlockingQueue<Runnable>(100);
     ThreadPoolExecutor executor;
+
     public void fillQueue(List<String> hashes) {
         for (Object hash : hashes) {
             WorkQueue.add((String) hash);
         }
     }
+
     public void dispatch(int threads, long timeout) {
         executor = new TPE(threads, threads, 0L,
                 java.util.concurrent.TimeUnit.MILLISECONDS, WorkerQueue, timeout);
         while (!WorkQueue.isEmpty()) {
             Future<?> task;
             task = executor.submit(new UnHash(WorkQueue.poll()));
-            try{
+            try {
                 task.get();
-            }
-            catch(Exception e){
+            } catch (Exception e) {
             }
         }
         executor.shutdown();
     }
+
     public static void main(String[] args) {
         try {
             List<String> hashes = Files.readAllLines(Paths.get(args[0]));
             int N = Integer.parseInt(args[1]);
-            long timeout = 0;
-            if (args.length>2) timeout = Long.parseLong(args[2]);
+            long timeout = -1;
+            if (args.length > 2)
+                timeout = Long.parseLong(args[2]);
             Dispatcher dispatcher = new Dispatcher();
             dispatcher.fillQueue(hashes);
             dispatcher.dispatch(N, timeout);
